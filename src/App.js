7d19b20f2ef9bbded84f4e075a377372c09ab450
Aristotelis <aristotelis.liakatas@gmail.com>
2023-10-14 10:05:43 +0100

    Adds randomiser function and basic working variables.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
 
+import random from 'random';
+
+function createRandomGroups(n, groupSize, allowLess = false) {
+  const names = Array.from({ length: n }, (_, i) => (i + 1).toString());
+  const groups = [];
+
+  if (groupSize > n) {
+    return groups;
+  }
+
+  while (n >= groupSize) {
+    const group = [];
+    for (let i = 0; i < groupSize; i++) {
+      const nameIndex = random.int(0, names.length - 1);
+      const name = names.splice(nameIndex, 1)[0];
+      group.push(name);
+    }
+
+    groups.push(group);
+    n = names.length;
+  }
+
+  if (allowLess) {
+    groups.push(...names);
+  } else {
+    const numGroups = groups.length;
+    for (let i = 0; i < names.length; i++) {
+      const groupIndex = i % numGroups;
+      groups[groupIndex].push(names[i]);
+    }
+  }
+
+  return groups;
+}
+
 function App() {
+  const [num1, setNumOfStudents] = useState(1);
+  const [num2, setNumOfGroups] = useState(1);
+  const [allowLess, setAllowLess] = useState(false);
+  const [groups, setGroups] = useState([]);
+  
+
+
+
+
+
   return (
     <div className="App">
       <header className="App-header">
